33cc0eb2364f53b8d2a1fe9bcf97519c00dd0402
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-10 10:14:25 +0100

    Modification du modèle de données, ajout des classes en fonctions du diagramme de classes

--- echange_competences/app/models.py
+++ echange_competences/app/models.py
@@ -1,18 +1,49 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+"""
+Implémentation des modèles de données en fonction du diagramme de classes
+
+"""
+# Création de la classe compétence
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
 
     def __str__(self):
         return self.name
 
-class Slot(models.Model):
-    date = models.DateField()
-    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+# La classe utilisateur
+
+class Profil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skill, blank=True)
 
     def __str__(self):
-        return self.date
+        return self.user.username
+
+# La classe activité ou demande d'aide
+
+class RequestHelp(models.Model):
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
+    required_skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    description = models.TextField()
+    slot = models.DateField()
+    help_found = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.description
+
+# La classe Proposition d'aide (l'utilisateur se rend disponible pour aider)
+
+class OfferHelp(models.Model):
+    request = models.OneToOneField(RequestHelp, on_delete=models.CASCADE)
+    helper = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Aide pour {self.request}"
+
+
+
 
