d70bda9e9a948e91eb105f52381f8344b6af8fc4
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-10 10:09:01 +0100

    Enregistrement des différentes classes dans admin.py

--- echange_competences/app/admin.py
+++ echange_competences/app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Skill, Slot
+from .models import Skill, Profil, RequestHelp, OfferHelp
 
 # Register your models here.
 admin.site.register(Skill)
-admin.site.register(Slot)
+admin.site.register(Profil)
+admin.site.register(RequestHelp)
+admin.site.register(OfferHelp)
