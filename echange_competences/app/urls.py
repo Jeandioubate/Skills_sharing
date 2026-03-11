463dc63bb1a58957c3c383ece9f5e475219dbc9e
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-11 15:50:10 +0100

    Ajout des différentes urls dans le fichier urls.py

--- echange_competences/app/urls.py
+++ echange_competences/app/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
 
+from django.urls import path
 from . import views
 
+"""
+Mise en place des URLs pour chaque vue
+"""
+
+app_name = "app"
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("skills/", views.skills_list, name="skills_list"),
+    path("my-skills/", views.my_skills, name="my_skills"),
+    path("demande/", views.create_request, name="create_request"),
+    path("demandes/", views.request_available, name="request_available"),
+    path("offer_help/<int:demande_id>/", views.offer_help, name="offer_help"),
 ]
