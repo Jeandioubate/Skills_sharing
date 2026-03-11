e7f442c5cbf921c2fb73135157de44ee2eb958ba
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-11 15:19:44 +0100

    Modification du fichier admin.py

--- echange_competences/app/admin.py
+++ echange_competences/app/admin.py
@@ -1,9 +1,25 @@
-from django.contrib import admin
 
+from django.contrib import admin
 from .models import Skill, Profil, RequestHelp, OfferHelp
 
-# Register your models here.
-admin.site.register(Skill)
-admin.site.register(Profil)
-admin.site.register(RequestHelp)
-admin.site.register(OfferHelp)
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Profil)
+class ProfilAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+    filter_horizontal = ("skills",)
+
+
+@admin.register(RequestHelp)
+class RequestHelpAdmin(admin.ModelAdmin):
+    list_display = ("description", "required_skill", "slot", "help_found")
+    list_filter = ("required_skill", "help_found")
+
+
+@admin.register(OfferHelp)
+class OfferHelpAdmin(admin.ModelAdmin):
+    list_display = ("request", "helper")
