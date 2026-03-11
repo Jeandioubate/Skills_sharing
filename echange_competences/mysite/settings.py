23eec84be4f635b65897ef4652e985a01cb656f6
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-11 17:40:44 +0100

    Modification des redirections dans settings.py

--- echange_competences/mysite/settings.py
+++ echange_competences/mysite/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "/app/"
-LOGOUT_REDIRECT_URL = "/app/"
-LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = '/app/'
+LOGOUT_REDIRECT_URL = '/app/'
+LOGIN_URL = '/login/'
