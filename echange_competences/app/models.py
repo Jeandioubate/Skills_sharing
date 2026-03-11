792d909dcc1d3987d4323731663f345d4665b82c
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-11 15:25:12 +0100

    Ajout de la fonction 'def create_user_profile' dans le fichier models.py pour la création automatique du profil

--- echange_competences/app/models.py
+++ echange_competences/app/models.py
@@ -1,48 +1,76 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-"""
-Implémentation des modèles de données en fonction du diagramme de classes
 
-"""
-# Création de la classe compétence
+# Classe compétence
 
 class Skill(models.Model):
+
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-# La classe utilisateur
+
+# Profil utilisateur
 
 class Profil(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     skills = models.ManyToManyField(Skill, blank=True)
 
     def __str__(self):
         return self.user.username
 
-# La classe activité ou demande d'aide
+
+# Demande d'aide
 
 class RequestHelp(models.Model):
+
     applicant = models.ForeignKey(User, on_delete=models.CASCADE)
+
     required_skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
     description = models.TextField()
+
     slot = models.DateField()
+
     help_found = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.description
 
-# La classe Proposition d'aide (l'utilisateur se rend disponible pour aider)
+        return f"{self.description} ({self.slot})"
+
+
+# Proposition d'aide
 
 class OfferHelp(models.Model):
+
     request = models.OneToOneField(RequestHelp, on_delete=models.CASCADE)
+
     helper = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
+
         return f"Aide pour {self.request}"
+
+
+# Création automatique du profil
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+
+    if created:
+
+        Profil.objects.create(user=instance)
+
+
+
+
 
 
 
