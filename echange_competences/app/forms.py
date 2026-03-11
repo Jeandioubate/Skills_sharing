000587389090585a247bc2bbd59cc5f7e38dfe58
Jean Dioubaté <jean.dioubate@fms-ea.com>
2026-03-11 15:22:50 +0100

    Modification du fichier forms.py, correction de l'attribut skill par required_skill

--- echange_competences/app/forms.py
+++ echange_competences/app/forms.py
@@ -3,27 +3,30 @@ from django import forms
 from .models import RequestHelp, Profil
 
 """
-Implémentation des formulaires dans forms.py
+Implémentation des formulaires
 """
 
-# Formulaire demande d'aide ou activité
+# Formulaire demande d'aide
 
 class RequestHelpForm(forms.ModelForm):
 
     class Meta:
+
         model = RequestHelp
 
         fields = [
-            "skill",
+            "required_skill",
             "description",
             "slot"
         ]
 
-# Formulaire pour renseigner les compétences
 
-class SkillForm(forms.ModelForm):
+# Formulaire gestion des compétences
+
+class CompetenceForm(forms.ModelForm):
 
     class Meta:
+
         model = Profil
 
         fields = ["skills"]
